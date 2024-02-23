98662fff3f299adc173f689eb98c1d3526cb37a5
Mezsoa <johnmessoa@gmail.com>
2024-02-23 19:48:57 +0100

    creating a helicopter using println and a while loop

---
+++ src/ROFLCOPTER.java
@@ -0,0 +1,58 @@
+public class ROFLCOPTER {
+    public static void main(String[] args) throws InterruptedException {
+
+        while (true) {
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("              :LOL:ROFL:ROFL                        ");
+            System.out.println("                A            ");
+            System.out.println("        /-----------------\\                      ");
+            System.out.println(" LOL=====                [ ]\\                       ");
+            System.out.println("  L     \\                   \\    ");
+            System.out.println("         \\___________________|   ");
+            System.out.println("                I      I           ");
+            System.out.println("          ______________________/                  ");
+            System.out.println("                        ");
+            System.out.println("                        ");
+            Thread.sleep(30);
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+
+
+            System.out.println("     ROFL:ROFL:LOL:                       ");
+            System.out.println("                A            ");
+            System.out.println("  L     /-----------------\\                      ");
+            System.out.println(" LOL=====                [ ]\\                       ");
+            System.out.println("        \\                   \\    ");
+            System.out.println("         \\___________________|   ");
+            System.out.println("                I      I           ");
+            System.out.println("          ______________________/                  ");
+            System.out.println("                        ");
+            System.out.println("                        ");
+
+            Thread.sleep(29);
+        }
+
+    }
+}
